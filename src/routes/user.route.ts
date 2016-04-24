c5cfecd68d90396fde6f324fca40d8c6ab3a7417
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-04-24 19:28:06 +0200

    changes user search route to work with url params

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,27 +1,52 @@
 import { Application, Request, Response } from 'express';
 import { UserTableController } from '../DataStore/UserTableController';
-import { SearchArgument } from '../DTOs/SearchArgument';
+import { SearchArgument, SortDirection } from '../DTOs/SearchArgument';
+
+import { Utils } from '../Utils';
 
 export function configureRoute(app : Application)
 {
-    app.get('/user/:query', route);
+    app.get('/user', search);
 }
 
-function route(req : Request, resp : Response)
+function search(req : Request, resp : Response)
 {
     let databaseSource = new UserTableController();
     let argument = new SearchArgument();
-    argument.query = req.params.query;
+        
+    if(req.query.query)
+    {
+        argument.query = req.query.query;   
+    }
+    if(req.query.skip && Utils.isPositiveInteger(req.query.skip))
+    {
+        argument.skip = Number(req.query.skip);
+    }
+    if(req.query.take && Utils.isPositiveInteger(req.query.take))
+    {
+        argument.take = Number(req.query.take);
+    }
+    if(req.query.sortDirection && Utils.isPositiveInteger(req.query.sortDirection))
+    {
+        let sortD = Number(req.query.sortDirection);
+        argument.sortDirection = Math.min(sortD, SortDirection.Descending);
+    }
+    if(req.query.sortColumn && Utils.arrayContains(databaseSource.sortColumnOptions, req.query.sortColumn))
+    { 
+        argument.sortColumn = req.query.sortColumn;
+    } 
+    else
+    {
+        argument.sortColumn = databaseSource.defaultSortColumn;    
+    }
            
     databaseSource.connect((err : Error) => {
         let res = databaseSource.search(argument, (users) => {
             var data = "";
             users.forEach(user => {
-                data = data + "\n" + user.email;
-                //resp.write(company.toString());
+                data = `${data} \n ${user.toString()}`;
             }); 
                 
-            console.log("klaar");
             databaseSource.close();
             
             resp.send(data);
