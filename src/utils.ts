021d9620ca1fd1e37c2b13c513e57de15ab60e9a
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-04-24 19:24:10 +0200

    adds utility class

---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+export class Utils{
+    public static isPositiveInteger(obj: any) : boolean {
+        let nr = parseFloat(obj);
+        
+        if(!isNaN(nr))
+        {
+            if(isFinite(nr))
+            {
+                if(nr >= 0)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+    public static arrayContains(array: any[], obj: any)
+    {
+        return array.indexOf(obj) > -1;
+    }
+}
