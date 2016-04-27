9cb787c28673b52d00d36e0950e04f926e176f1b
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-04-27 19:11:55 +0200

    updates user list example

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -34,12 +34,11 @@ function search(req: Request, resp: Response) {
 
     databaseSource.connect((err: Error) => {
         let res = databaseSource.search(argument, (users) => {
-            var data = "resultaten: " + users.count;
+            var data = `resultaten:  <b>${ users.count }</b><lu> `;
             users.results.forEach(user => {
-                data = `${data}
-                ${user.toString()}`;
+                data += `<li>${user.toString()}</li>`;
             });
-
+            data += '</ul>'
             databaseSource.close();
 
             resp.send(data);
