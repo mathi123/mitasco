7c16e113ce4371e3cb1bf0827adfe18bcb1eb6f6
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-07-31 12:09:44 +0200

    Angular2 client setup (#14)
    * sets up database table
    
    * adds dto
    
    * adds routes and table controller
    
    * moves server files
    
    * first client configuration

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,14 @@ if(test()){
   environment = 'production';
 }
 
+gulp.task('default', function (callback) {
+    runSequence('build-server', 'build-client', callback);
+});
+
+/////////////////////////
+// SERVER CONFIGURATION
+/////////////////////////
+
 gulp.task('copy-db-config', function () {
   console.info("Copy pgconf.json to bin folder");
 
@@ -40,16 +48,22 @@ gulp.task('copy-db-config', function () {
   }
 });
 
-gulp.task('build', function(callback){
-  runSequence('clean', 'compile', 'copy-db-config', callback);
+gulp.task('build-server', function(callback){
+  runSequence('clean-server', 'compile-server', 'copy-db-config', callback);
 });
 
-gulp.task('clean', function () {
+gulp.task('clean-server', function () {
   del([config[environment].buildDir], {force: true});
 });
 
-gulp.task('compile', function () {
-  return gulp.src(['src/**/*.ts', 'typings/**/*.d.ts'])
+gulp.task('compile-server', function () {
+  return gulp.src(['src/server/**/*.ts', 'typings/globals/pg-promise/*.d.ts',
+          'typings/globals/body-parser/*.d.ts','typings/globals/express/*.d.ts',
+          'typings/globals/express-serve-static-core/*.d.ts',
+          'typings/globals/node/*.d.ts', 'typings/globals/serve-static/*.d.ts',
+          'typings/globals/spex/*.d.ts', 'typings/globals/mime/*.d.ts',
+          'typings/modules/pg/*.d.ts'
+  ])
       .pipe(development(ts({
         target: "ES6",
         module: "commonjs",
@@ -67,6 +81,64 @@ gulp.task('compile', function () {
       })))
       .pipe(gulp.dest(config[environment].buildDir));
 });
+
+////////////////////////
+// CLIENT CONFIGURATION
+////////////////////////
+
+gulp.task('build-client', function (callback) {
+   runSequence('clean-client', 'copy-imwa', 'copy-rxjs', 'copy-libs', 'copy-angular-libs', 'copy-other', 'compile-client', callback);
+});
+gulp.task('clean-client', function (callback) {
+    del([path.join(config[environment].buildDir, 'app')], {force: true});
+    callback();
+});
+gulp.task('copy-angular-libs', function () {
+    return gulp.src(['node_modules/@angular/**'])
+        .pipe(gulp.dest(path.join(config[environment].buildDir, 'app', 'libs', 'angular2')));
+});
+gulp.task('copy-imwa', function () {
+    return gulp.src(['node_modules/angular2-in-memory-web-api/**'])
+        .pipe(gulp.dest(path.join(config[environment].buildDir, 'app', 'libs', 'angular2-in-memory-web-api')));
+});
+gulp.task('copy-rxjs', function () {
+    return gulp.src(['node_modules/rxjs/**'])
+        .pipe(gulp.dest(path.join(config[environment].buildDir, 'app', 'libs', 'rxjs')));
+});
+gulp.task('copy-libs', function () {
+    return gulp.src(['node_modules/core-js/client/shim.min.js',
+        'node_modules/zone.js/dist/zone.js',
+        'node_modules/reflect-metadata/Reflect.js',
+        'node_modules/systemjs/dist/system.src.js'])
+            .pipe(gulp.dest(path.join(config[environment].buildDir, 'app', 'libs')));
+});
+gulp.task('copy-other', function () {
+   return gulp.src(['src/client/**/*.html', 'src/client/**/*.css', 'src/client/**/*.conf.js'])
+       .pipe(gulp.dest(path.join(config[environment].buildDir, 'app')));
+});
+gulp.task('compile-client', function () {
+    return gulp.src(['src/client/**/*.ts',
+            'typings/globals/core-js/*.d.ts',
+            'typings/globals/jasmine/*.d.ts',
+            'typings/globals/node/*.d.ts'
+    ])
+        .pipe(development(ts({
+            target: "ES5",
+            module: "commonjs",
+            sourceMap: true,
+            emitDecoratorMetadata: true,
+            experimentalDecorators: true,
+            removeComments: false,
+            noImplicitAny: false,
+            moduleResolution: "node"
+        })))
+        .pipe(gulp.dest(path.join(config[environment].buildDir, 'app')));
+});
+
+
+///////////////////////
+/// TESTING
+///////////////////////
 
 gulp.task('test', function (callback) {
     runSequence('copy-tests', 'run-tests', callback);
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,20 +3,28 @@ import * as http from "http";
 import * as bodyParser from "body-parser";
 import * as userRoutes from "./routes/user.route";
 import * as indexRoutes from "./routes/index.route";
+import * as todoRoutes from "./routes/todo.route";
 
 class StartUp {
     public static main(): number {
-        let app = new express;
+        var app = express();
 
         app.set('port', process.env.PORT || 3000);
         app.use(bodyParser.json());
 
         // Configure routes
         app.get('/', indexRoutes.route);
+
         app.get('/user', userRoutes.search);
         app.get('/user/:id', userRoutes.read);
         app.post('/user', userRoutes.create);
         app.put('/user', userRoutes.create);
+
+        app.get('/todo', todoRoutes.search);
+        app.get('/todo/:id', todoRoutes.read);
+        app.post('/todo', todoRoutes.create);
+        app.put('/todo', todoRoutes.create);
+        app.use('/app', express.static(__dirname + '/app'));
 
         http.createServer(app).listen(app.get('port'), function () {
             console.log("Express server listening on port " + app.get('port'));
