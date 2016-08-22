7167f4aa9b947dea0dc91d4a5011ceb7d0d48949
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-08-22 08:06:13 +0200

    updates usercontroller tests

--- test/server/user-controller.test.js
+++ test/server/user-controller.test.js
@@ -3,45 +3,37 @@ var should = require("should");
 
 describe("UserController class", function () {
     var userToAdd = {
-        fullname: "Mathias Colpaert",
+        fullname: "John Doe",
         email: Math.random() + "@gmail.com"
     };
+    var controller = new UserController();
 
-    describe("when created", function () {
-        var controller;
-        it("calls constructor correctly", function () {
-            controller = new UserController();
-        });
-        it("can run a query", function (done) {
-            var searchArg = {
-                query: "at",
-                skip: 0,
-                take: 50,
-                sortColumn: "name",
-                sortDirection: 0
-            };
+    it("can run a search query", function (done) {
+        var searchArg = {
+            query: "at",
+            skip: 0,
+            take: 50,
+            sortColumn: "name",
+            sortDirection: 0
+        };
 
-            controller.search(searchArg)
-                .then(function (data) {
-                    done();
-                })
-                .catch(function (err) {
-                    done(err);
-                });
-        });
+        controller.search(searchArg)
+            .then(function (data) {
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
     });
 
-    describe("when create is called", function () {
-        var controller = new UserController();
-
+    describe("when a user is created", function () {
         it("an id is returned", function (done) {
-            controller.create(userToAdd, "test").then(function (id) {
-                console.info("create succes " + id);
-                id.should.not.equal(0);
+            controller.create(userToAdd, "test")
+                .then(function (id) {
+                    id.should.not.equal(0);
                     done();
-                })
-                .catch(function (err) {
-                done(err);
+                }).catch(function (err) {
+                    done(err);
                 });
         });
     });
