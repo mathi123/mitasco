d5920221f5da1e97e16e5599b4924e5105c0851e
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-09-11 12:08:16 +0200

    adds token generator to test helpers

--- src/server/test-helpers.ts
+++ src/server/test-helpers.ts
@@ -1,4 +1,4 @@
-import { User } from "../client/shared/user";
+import { User } from "./shared/user";
 import { UserController } from "./controllers/user.controller";
 import { GroupController } from "./controllers/group.controller";
 import { Permissions } from "./security/permissions";
@@ -6,8 +6,10 @@ import { KeyValuePair } from "./shared/key-value-pair";
 import { PermissionCodeController } from "./controllers/permission-code.controller";
 import { PermissionCode } from "./shared/permission-code";
 import { Group } from "./shared/group";
+import { AuthenticationController } from "./controllers/authentication.controller";
+
 export class TestHelpers{
-    public static async CreateUser():Promise<User>{
+    public static async createUser():Promise<User>{
         let controller = new UserController();
         let user = new User();
         user.email = Math.random() + "@gmail.com";
@@ -17,8 +19,8 @@ export class TestHelpers{
         user.id = id;
         return user;
     }
-    public static async CreateAdminUser():Promise<User>{
-        let user = await this.CreateUser();
+    public static async createAdminUser():Promise<User>{
+        let user = await this.createUser();
         let userKv = new KeyValuePair();
         userKv.key = user.id;
         userKv.value = user.fullname;
@@ -30,14 +32,14 @@ export class TestHelpers{
         for(let group of groups){
             if(found) break;
 
-            for(let perm of group.permissionCodes){
+            let fullgroup = await controller.read(group.id);
+            for(let perm of fullgroup.permissionCodes){
                 if(found) break;
 
                 if(perm.code == Permissions.Admin){
                     found = true;
-
-                    group.users.push(userKv);
-                    await controller.update(group);
+                    fullgroup.users.push(userKv);
+                    await controller.update(fullgroup);
                 }
             }
         }
@@ -63,5 +65,14 @@ export class TestHelpers{
 
         return user;
     }
-
+    public static async getToken():Promise<string>{
+        let user = await this.createUser();
+        let auth = new AuthenticationController();
+        return await auth.createToken(user.id);
+    }
+    public static async getAdminToken():Promise<string>{
+        let user = await this.createAdminUser();
+        let auth = new AuthenticationController();
+        return await auth.createToken(user.id);
+    }
 }
