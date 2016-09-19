ddb53179b6da35b10ba037a9ae5f34b0637c41d2
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-09-19 21:25:57 +0200

    extract menu template

--- src/client/components/menu/menu.component.ts
+++ src/client/components/menu/menu.component.ts
@@ -4,25 +4,7 @@ import { MenuProvider } from "../../providers/menu.provider";
 @Component({
     moduleId: module.id,
     selector: 'menu',
-    template: `
-        <nav [class.open]="isOpen"
-             [class.closed]="!isOpen">
-            <accordion>
-                <accordion-group heading="Algemeen" isGroupOpen="true">
-                    <ul>
-                        <li><p routerLink="/todo-list">Todo's</p></li>
-                    </ul>
-                </accordion-group>
-                <accordion-group heading="Admin" isGroupOpen="true">
-                    <ul>
-                        <li><p routerLink="/group-list">Groepen</p></li>
-                        <li><p>Gebruikers</p></li>
-                        <li><p>Permissies</p></li>
-                    </ul>
-                </accordion-group>
-            </accordion>
-        </nav>
-    `
+    templateUrl: 'menu.template.html'
 })
 export class MenuComponent {
     public isOpen:boolean = true;
