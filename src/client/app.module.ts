b2827ded947016b1e30b8ea3cc784564318c1d95
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-09-24 15:44:19 +0200

    -

--- src/client/app.module.ts
+++ src/client/app.module.ts
@@ -22,14 +22,16 @@ import { MenuComponent } from "./components/menu/menu.component";
 import { UrlTrackingService } from "./services/url-tracking.service";
 import { SuggestionInputComponent } from "./components/suggestion-input/suggestion-input.component";
 import { UserService } from "./services/user.service";
+import { PermissionCodeService } from "./services/permission-code.service";
+import { PermissionCodeListComponent } from "./components/permission-code/permission-code-list.component";
 
 @NgModule({
     imports: [ BrowserModule, FormsModule, HttpModule, routing ],
     declarations: [ LoginComponent, ApplicationRootComponent, MenuComponent, TodoListComponent, TodoDetailComponent,
         DashboardComponent, GroupListComponent,GroupDetailComponent,AccordionComponent,AccordionGroupComponent,
-        PageNotFoundComponent, SuggestionInputComponent],
+        PageNotFoundComponent, SuggestionInputComponent, PermissionCodeListComponent],
     providers: [TodoService,GroupService, appRoutingProviders, ConfigurationProvider,
-        AuthenticationService, MenuProvider,UrlTrackingService, UserService],
+        AuthenticationService, MenuProvider,UrlTrackingService, UserService, PermissionCodeService],
     bootstrap: [ApplicationRootComponent]
 })
 export class AppModule { }
---
+++ src/client/components/permission-code/permission-code-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { PermissionCodeService } from "../../services/permission-code.service";
+import { PermissionCode } from "../../shared/permission-code";
+import { ConfigurationProvider } from "../../providers/configuration.provider";
+import { Router } from "@angular/router";
+
+@Component({
+    moduleId: module.id,
+    selector: 'permission-code-list',
+    templateUrl: 'permission-code-list.template.html'
+})
+export class PermissionCodeListComponent implements OnInit {
+    private records: PermissionCode[];
+
+    constructor(private service:PermissionCodeService, private configuration:ConfigurationProvider, private router:Router) {
+
+    }
+
+    ngOnInit() {
+        if(!this.configuration.isLoggedIn()){
+            this.router.navigate(['/login']);
+            return;
+        }else{
+            this.loadData();
+        }
+    }
+
+    private loadData(){
+        this.service.getAll()
+            .then((data:PermissionCode[]) => this.records = data)
+            .catch((err) => console.log(err));
+    }
+
+}
