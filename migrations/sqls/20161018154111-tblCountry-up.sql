cf3cc354de48c4a7473c792dc26847416a8293b6
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-10-18 19:15:27 +0200

    adds country seed function

--- migrations/sqls/20161018154111-tblCountry-up.sql
+++ migrations/sqls/20161018154111-tblCountry-up.sql
@@ -3,7 +3,8 @@ CREATE TABLE country
 (
     id SERIAL PRIMARY KEY NOT NULL,
     code VARCHAR(2) NOT NULL,
-    name_nl VARCHAR(300) NOT NULL
+    name_nl VARCHAR(300) NOT NULL,
+    priority BOOLEAN NOT NULL DEFAULT FALSE
 );
 CREATE UNIQUE INDEX country_id_uindex ON country (id);
 CREATE UNIQUE INDEX country_code_uindex ON country (code);
