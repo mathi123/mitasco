9391ada5ef01336f71030c46eb2db4d4e1261c5c
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-10-18 17:44:36 +0200

    adds country table

---
+++ migrations/sqls/20161018154111-tblCountry-up.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE country
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    code VARCHAR(2) NOT NULL,
+    name_nl VARCHAR(300) NOT NULL
+);
+CREATE UNIQUE INDEX country_id_uindex ON country (id);
+CREATE UNIQUE INDEX country_code_uindex ON country (code);
