1ca661b3659c9f0754937fa2a953e192216c6060
Mathias Colpaert <colpaert.mathias@gmail.com>
2016-11-05 10:23:32 +0100

    add modals (#75)
    * copies angular bundles only to dist
    
    * adds modal module
    
    * adds modal styling
    
    * bugfix
    
    * adds yes-no dialog modal
    
    * add modal styling and cancel button
    
    * add minimal styling

---
+++ src/client/modals/yes-no-dialog/yes-no-dialog-component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { ModalBase } from "../modal-base";
+import { DialogResult } from "./dialog-result";
+
+@Component({
+    moduleId: module.id,
+    selector: 'yes-no-dialog',
+    templateUrl: 'yes-no-dialog.component.html'
+})
+export class YesNoDialogComponent extends ModalBase implements OnInit {
+    @Input()
+    private question: string;
+
+    @Output() dialogClosed = new EventEmitter();
+
+    constructor() {
+        super();
+    }
+
+    ngOnInit() {
+    }
+
+    private yes() {
+        this.closeWithResult(DialogResult.Yes);
+    }
+
+    private no() {
+        this.closeWithResult(DialogResult.No);
+    }
+
+    private cancel() {
+        this.closeWithResult(DialogResult.Cancel);
+    }
+
+    private closeWithResult(result: DialogResult) {
+        this.dialogClosed.emit(result);
+        this.closeModal();
+    }
+}
